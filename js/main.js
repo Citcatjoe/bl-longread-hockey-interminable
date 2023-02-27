a7df1a1c16ae2c8fbd44d0bea2b58dbc39fbb552
César Greppin <Citcatjoe@users.noreply.github.com>
2023-02-27 10:16:33 +0100

    init from drogue

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function($){
+
+	// data = [
+    //     { 
+    //         title: 'titre 1',
+    //         chapeau: 'chapeau 1'
+    //     },
+    //     { 
+    //         title: 'titre 2',
+    //         chapeau: 'chapeau 2'
+    //     }	
+	// ];
+	// console.log(data);
+
+    // buildCards(data);
+    // function buildCards(data){
+    //     var source   = $("#card-template").html();
+    //     var template = Handlebars.compile(source);
+    //     var html = template({'card':data});
+    //     $(".authors").prepend(html); 
+    // }
+    setTimeout(function() { 
+        $('body').addClass('is-visible');
+    }, 1000);
+    
+
+	
+
+})(jQuery);
